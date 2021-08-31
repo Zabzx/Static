406dddf85572606c62a50ca43884a41e91f61d63
Zabzx <88985587+Zabzx@users.noreply.github.com>
2021-08-30 22:36:40 -0400

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,30 @@
+//Header scroll
+const headerContainer = document.querySelector('.header-container');
+const headerLinks = document.querySelectorAll('.header-link');
+
+window.addEventListener('scroll', () => {
+    headerContainer.classList.toggle('header-sticky', window.scrollY > 50);
+
+    if(screen.width < 700){
+        headerContainer.classList.remove('header-sticky');
+        return;
+    }
+
+    if (window.scrollY > 50){
+        headerLinks.forEach(link => {
+            link.classList.add('link-black')
+        });
+    } else {
+        headerLinks.forEach(link => {
+            link.classList.remove('link-black');
+        });
+    };
+});
+
+//Navbar
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
