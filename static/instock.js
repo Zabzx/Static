6d9987c4ef6c96aec62685fa5f1a6813d488c7cd
Zabzx <88985587+Zabzx@users.noreply.github.com>
2021-09-01 17:49:31 -0400

    Update instock.js

--- static/instock.js
+++ static/instock.js
@@ -101,3 +101,11 @@ filterBtns.forEach(btn => {
         }
     })
 })
+
+//Navbar
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
